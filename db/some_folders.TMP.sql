17332b7e498b24ff42221ed046ff11df814bc7e3
Angel Galindo <zoquero@gmail.com>
2017-08-10 17:28:05 +0200

    develop: There was still an 'enabled' attribute on two sql scripts for introducing debugging data

--- db/some_folders.TMP.sql
+++ db/some_folders.TMP.sql
@@ -1,7 +1,7 @@
-INSERT INTO `folder` (`name`, `moref`, `parent`, `enabled`) VALUES ('Datacenters',  'group-d1',  '0', '1');
-INSERT INTO `folder` (`name`, `moref`, `parent`, `enabled`) VALUES ('datastore', 'group-s5', '0', '1');
-INSERT INTO `folder` (`name`, `moref`, `parent`, `enabled`) VALUES ('host', 'group-h4', '0', '1');
-INSERT INTO `folder` (`name`, `moref`, `parent`, `enabled`) VALUES ('network', 'group-n6', '0', '1');
-INSERT INTO `folder` (`name`, `moref`, `parent`, `enabled`) VALUES ('vm', 'group-v3', '0', '1');
-INSERT INTO `folder` (`name`, `moref`, `parent`, `enabled`) VALUES ('erp', 'group-v9653', '2', '1');
-INSERT INTO `folder` (`name`, `moref`, `parent`, `enabled`) VALUES ('www', 'group-v9659', '2', '1');
+INSERT INTO `folder` (`name`, `moref`, `parent`) VALUES ('Datacenters',  'group-d1',  '0');
+INSERT INTO `folder` (`name`, `moref`, `parent`) VALUES ('datastore', 'group-s5', '0');
+INSERT INTO `folder` (`name`, `moref`, `parent`) VALUES ('host', 'group-h4', '0');
+INSERT INTO `folder` (`name`, `moref`, `parent`) VALUES ('network', 'group-n6', '0');
+INSERT INTO `folder` (`name`, `moref`, `parent`) VALUES ('vm', 'group-v3', '0');
+INSERT INTO `folder` (`name`, `moref`, `parent`) VALUES ('erp', 'group-v9653', '2');
+INSERT INTO `folder` (`name`, `moref`, `parent`) VALUES ('www', 'group-v9659', '2');
